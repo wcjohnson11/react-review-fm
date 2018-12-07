54e48a78c6fe5cb86103799344250ecab2dab022
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-07 15:32:13 -0500

    add Pets component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.species),
+        React.createElement("h2", {}, props.breed)
+    ])
+}
+
 const App = () => {
-    return React.createElement("div", {},
-        React.createElement("h1", {}, "Adopt Me!")
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "spot",
+            species: "dog",
+            breed: "dalmatian"
+        }),
+        React.createElement(Pet, {
+            name: "luna",
+            species: "cat",
+            breed: "black"
+        }),
+        React.createElement(Pet, {
+            name: "remy ma",
+            species: "dog",
+            breed: "french bulldog"
+        })
+        ]
     )
 }
 
