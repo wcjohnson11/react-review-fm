5a40de9da6d457682b76729207bc169be168fac9
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-10 17:05:13 -0500

    break Pet componenet into own file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.species),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
   handleTitleClick() {
