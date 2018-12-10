840ccc317230e3af9e5fa6fdff42c1162cb83b09
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-10 17:16:41 -0500

    change App createElement calls to jsx

--- src/App.js
+++ src/App.js
@@ -7,28 +7,37 @@ class App extends React.Component {
     alert("You clicked the title");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "spot",
-        species: "dog",
-        breed: "dalmatian"
-      }),
-      React.createElement(Pet, {
-        name: "luna",
-        species: "cat",
-        breed: "black"
-      }),
-      React.createElement(Pet, {
-        name: "cremy ma",
-        species: "dog",
-        breed: "french bulldog"
-      })
-    ]);
+    // return React.createElement("div", {}, [
+    //   React.createElement(
+    //     "h1",
+    //     { onClick: this.handleTitleClick },
+    //     "Adopt Me!"
+    //   ),
+    //   React.createElement(Pet, {
+    //     name: "spot",
+    //     species: "dog",
+    //     breed: "dalmatian"
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "luna",
+    //     species: "cat",
+    //     breed: "black"
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "cremy ma",
+    //     species: "dog",
+    //     breed: "french bulldog"
+    //   })
+    // ]);
+    return (
+        <div>
+            <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+            <Pet name="Spot" animal="Dog" breed="Dalmatian"/>
+            <Pet name="Luna" animal="Cat" breed="Mixed"/>
+            <Pet name="Cremy Ma" animal="Dog" breed="French Bulldog"/>
+        </div>
+    );
+
   }
 }
 
--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Pet = props => {
-    return (
-        <div>
-            <h1>{ props.name }</h1>
-            <h2>{ props.species }</h2>
-            <h2>{ props.breed }</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.species}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;
