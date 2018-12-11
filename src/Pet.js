b25f83dfd3f3f55a21a6dcaef83e013815ed9dd6
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-10 20:50:15 -0500

    adding pet component link to details tab

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 class Pet extends React.Component {
   render() {
-    const { name, animal, breed, media, location } = this.props;
+    const { id, name, animal, breed, media, location } = this.props;
 
     let photos = [];
 
@@ -10,7 +11,7 @@ class Pet extends React.Component {
       photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
     }
     return (
-      <div className="pet">
+      <Link to={`/details/${id}`} className="pet">
         <div className="image-container">
           <img src={photos[0].value} alt={name} />
         </div>
@@ -20,7 +21,7 @@ class Pet extends React.Component {
             {animal} - {breed} - {location}
           </h2>
         </div>
-      </div>
+      </Link>
     );
   }
 }
