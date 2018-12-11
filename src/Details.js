481f28a4c613ec3bd1d5d1b2edaf72a916b48101
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-10 21:15:18 -0500

    add pet text to details page

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import pf from 'petfinder-client';
+import { navigate } from '@reach/router/lib/history';
+
+const petfinder = pf({
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            loading: true
+        };
+    }
+    componentDidMount() {
+        petfinder.pet.get({
+            output: "full",
+            id: this.props.id
+        }).then(data => {
+            const { pet } = data.petfinder;
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+                breed = pet.breeds.breed.join(', ');
+            } else {
+                breed = pet.breeds.breed;
+            }
+
+            this.setState({
+                name: pet.name,
+                animal: pet.animal,
+                location: `${pet.contact.city}, ${pet.contact.state}`,
+                description: pet.description,
+                media: pet.media,
+                breed,
+                loading: false
+            });
+        }).catch(err => {
+            navigate("/");
+            this.setState({ error: err });
+        });
+    }
     render() {
+        if (this.state.loading) {
+            return <h1>loading ....</h1>
+        }
+
+        const { animal, breed, name, location, description } = this.state;
         return (
-            <div>
-                <h1> hi lollll</h1>
+            <div className="details">
+                <h1>{name}</h1>
+                <h2> {animal} - { breed } - { location }</h2>
+                <p>{description}</p>
             </div>
-        )
+        );
     }
 }
 
