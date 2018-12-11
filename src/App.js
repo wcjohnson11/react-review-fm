1f6b3b2ebf0411548153ab2e75b39dec3bc3bf28
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-10 20:42:04 -0500

    add reach router and details page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from '@reach/router';
+import Details from './Details';
 import Results from './Results';
-
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Results />
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
