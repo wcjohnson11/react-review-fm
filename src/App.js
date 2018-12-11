0b2a8812c0c36a69c78d49244c368b6dbf44dc99
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-11 16:47:27 -0500

    complete SearchBox component and add to Results page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Details from "./Details";
 import Results from "./Results";
 import SearchParams from "./SearchParams";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
   constructor(props) {
     super(props);
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,57 +1,11 @@
 import React from "react";
-import pf, { ANIMALS } from "petfinder-client";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import SearchBox from "./SearchBox";
 
 class SearchParams extends React.Component {
   render() {
     return (
-      <div className="search-params">
-        <label htmlFor="location">
-          Location
-          <input
-            onChange={this.handleLocationChange}
-            id="location"
-            value={this.state.location}
-            placeholder="Location"
-          />
-        </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={this.state.animal}
-            onChange={this.handleAnimalChange}
-            onBlur={this.handleAnimalChange}
-          >
-            <option />
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          breed
-          <select
-            id="breed"
-            value={this.state.breed}
-            onChange={this.handleBreedChange}
-            onBlur={this.handleBreedChange}
-            disabled={!this.state.breeds.length}
-          >
-            <option />
-            {this.state.breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
+      <div className="search-route">
+        <SearchBox />
       </div>
     );
   }
