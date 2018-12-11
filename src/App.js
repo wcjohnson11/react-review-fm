3048dee2cb72e9354e9a04f3fdd61616c440bdc8
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-10 19:14:30 -0500

    switch placeholder data for real data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from "react";
 import { render } from "react-dom";
-import pf from 'petfinder-client';
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
 const petfinder = pf({
-    key: process.env.API_KEY,
-    secret: process.env.API_SECRET
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
 });
 
 class App extends React.Component {
-  componentDidMount() {
-    const promise = petfinder.breed.list({ animal: "dog" });
+  constructor(props) {
+    super(props);
 
-    promise.then(console.log, console.error);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Brooklyn, NY" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({ pets });
+      });
   }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Spot" animal="Dog" breed="Dalmatian" />
-        <Pet name="Luna" animal="Cat" breed="Mixed" />
-        <Pet name="Cremy Ma" animal="Dog" breed="French Bulldog" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return <Pet animal={pet.animal} name={pet.name} breed={breed} />;
+          })}
+        </div>
       </div>
     );
   }
