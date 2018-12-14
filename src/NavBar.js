180ed555e0bd6d1790b9edf0744d8a9fd4d1c980
William Johnson <williamjohnson@Williams-MacBook-Air.local>
2018-12-13 21:55:43 -0500

    adding animation to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/core';
 import colors from './colors';
+
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const SpyGlass = styled("span")`
+  display: inline-block;
+  animation: 1s ${Spin} linear infinite;
+`;
 
 const Container = styled("header")`
     background-color: ${colors.dark};
@@ -20,9 +36,9 @@ const NavBar = () => (
   <Container>
     <NavLink to="/">Adopt Me!</NavLink>
     <NavLink to="/search-params">
-      <span aria-label="search" role="img">
+      <SpyGlass aria-label="search" role="img">
         🔍
-      </span>
+      </SpyGlass>
     </NavLink>
   </Container>
 );
