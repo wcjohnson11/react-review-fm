86d59e664f55a8859cb739c27722c997267a0c2f
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-15 21:24:29 -0500

    create redux store

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+const store = createStore(reducer, compose(
+    // Allow async
+    applyMiddleware(thunk),
+    // Ensure redux doesn't crash
+    typeof window === 'object' &&
+    typeof window.devToolsExtension !== "undefined" ? window.devToolsExtension()
+    :f => f
+));
+
+export default store;
